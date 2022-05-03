9fe6351bdcf146f22f22f67534ed3036f58ffda1
Omar Mokhtar <omokhtarm@gmail.com>
2022-05-04 01:50:33 +0200

    threaded server

--- server.py
+++ server.py
@@ -1,16 +1,26 @@
 import socket
+import threading
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
+def handle_conn(connection, address):
+    with connection:
+        print(f"Connected by {address}")
         while True:
-            data = conn.recv(1024)
+            data = connection.recv(1024)
+            print(f"Received {data!r}")
             if not data:
                 break
-            conn.sendall(data)
+            connection.sendall(data)
+
+
+if __name__ == '__main__':
+    HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+    PORT = 80  # Port to listen on (non-privileged ports are > 1023)
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        while True:
+            connection, address = s.accept()
+            th = threading.Thread(target=handle_conn, args=[connection, address])
+            th.start()

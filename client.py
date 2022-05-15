ade2bb1491c0aff49d5632e9179718db3cadf242
Amr Ayman <miroamr78@gmail.com>
2022-05-15 22:10:59 +0200

    final, rbna y5aleeny leekom

--- client.py
+++ client.py
@@ -1,17 +1,98 @@
 import socket
+import mimetypes
+from sys import argv
+from os import path, getcwd
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 80  # The port used by the server
+TIMEOUT = 10
 
-# file = open("logo.png", "rb")
-# byte = file.read(3)
-# while byte:
-#     print(byte)
-#     byte = file.read(3)
+input_file = argv[1] if len(argv) > 1 else 'test_requests.txt'
+input_file = open(input_file, 'r')
+prev_port = None
+sock = None
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall("POST bla bla bla".encode())
-    data = s.recv(1024)
+for request in input_file.readlines():
 
-print(f"Received {data!r}")
+    port = 80 # default port
+
+    s = request.split()
+    if len(s) == 4: method, filename, hostname, port = s
+    elif len(s) == 3: method, filename, hostname = s
+    else: continue # bad line
+
+    if method == 'POST' and not path.isfile(filename): continue
+
+    # valid request
+    port = int(port)
+    print(request)
+
+    while True: # just a goto
+        retry = False
+        out = False
+
+        # create a new connection or use old one?
+        if not sock or prev_port != port:
+            prev_port = port
+            if sock: sock.close()
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # new
+
+            try: sock.connect((hostname, port))
+            except ConnectionRefusedError:
+                sock = None
+                out = True
+                break # bad port
+
+            sock.settimeout(TIMEOUT)
+
+        prev_port = port
+
+        if method == 'GET':
+            first = True
+            ff = None
+            length_so_far = 0
+            length = None
+
+            sock.sendall(f'GET /{filename} HTTP/1.1\r\n'.encode())
+            while True:
+
+                try: data = sock.recv(1024)
+                except (TimeoutError, ConnectionResetError):
+                    retry = True
+                    break # must re-request
+                if not data: raise Exception('Very Weird') # should never happen
+
+                if first:
+                    data = data.split(b'\r\n\r\n')
+                    header = data[0].decode().split('\r\n')
+                    if header[0].split()[1] == '404': break
+                    if not length:
+                        for c in header:
+                            c = c.split(':')
+                            if c[0] == 'Content-Length':
+                                length = int(c[1])
+                                break
+
+                    if len(data) == 1: continue # long header
+                    data = data[1]
+                    first = False
+
+                if not ff: ff = open(filename, 'wb')
+                ff.write(data)
+
+                length_so_far += len(data)
+                if length_so_far == length: break # complete file received
+
+            if ff: ff.close()
+
+        else: # method == 'POST'
+            af = filename
+            filename = path.join(getcwd(), filename)
+            ff = open(filename, 'rb')
+            sock.sendall('POST /{} HTTP/1.1\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n'
+            .format(af, mimetypes.guess_type(filename)[0], path.getsize(filename)).encode() + ff.read())
+            ff.close()
+        
+        if out: continue
+        if not retry: break
+
+if s: sock.close()
+input_file.close()
--- server.py
+++ server.py
@@ -1,47 +1,85 @@
 import socket
 import threading
+import mimetypes
+from os import path
 
+HOST = '127.0.0.1'
+PORT = 80
+TIMEOUT = 300
 
-def handle_conn(connection, address):
-    with connection:
-        print(f"Connected by {address}")
-        while True:
-            data = connection.recv(1024)
-            parse_data(data)
-            # print(f"Received {data!r}")
-            if not data:
-                break
-            connection.sendall(data)
+cache = {}
 
+def handle_conn(conn, address):
+    conn.settimeout(TIMEOUT) # static timeout for persistent connections
+    print(f"Connected to {address}")
 
-def parse_data(data):
-    data = data.decode()
-    data = data.split("\r\n")
-    req_type = data[0].split()[0]
+    while True: # for persistent connections
 
-    if req_type == 'GET':
-        print("IN GET")
-    elif req_type == 'POST':
-        print("IN POST")
+        try: data = conn.recv(1024)
+        except (TimeoutError, ConnectionResetError): break
+        if not data: break
+
+        method, req, payload = parse_request_message(data)
+        target = path.join('public', path.basename(req.split()[1]))
+
+        print(f"Received a {method!r} request.")
+
+        # check the cache
+        a = cache.get(target)
+        if a:
+            print('Cache Hit.')
+            conn.sendall(a)
+            continue
+
+        # miss
+        if method == 'GET': handle_GET(target, conn)
+        elif method == 'POST': handle_POST(target, payload, conn)
+
+        # the request was served, wait for another
+
+    conn.close()
+
+def parse_request_message(data):
+    data = data.split(b'\r\n\r\n')
+
+    header = data[0].decode().split('\r\n')
+    request = header[0]
+    method = request.split()[0]
+
+    body = data[1] if len(data) > 1 else None
+
+    # print method and header options
+    print(method)
+    for line in header:
+        print(line)
+
+    return (method, request, body)
+
+def handle_GET(target, conn):
+    if path.isfile(target):
+        res = (b'HTTP/1.0 200 OK\r\n' + 'Content-Type: {}\r\nContent-Length: {}\r\n\r\n'
+        .format(mimetypes.guess_type(target)[0], path.getsize(target)).encode() +
+        open(target, 'rb').read())
     else:
-        print("BAD")
+        res = b'HTTP/1.0 404 Not Found\r\n\r\n'
+    
+    cache[target] = res
+    conn.sendall(res)
 
+def handle_POST(target, payload, conn):
+    with open(target, 'wb') as f:
+        while payload is not None:
+            f.write(payload)
+            try: payload = conn.recv(1024)
+            except (TimeoutError, ConnectionResetError): break
 
-def handle_GET(request):
-    # TODO: handle get
-    pass
-
-
-def handle_POST(request):
-    # TODO: handle post
-    pass
-
+    conn.sendall(b'HTTP/1.0 200 OK\r\n')
+    print(f'Created {target!r}')
 
 if __name__ == '__main__':
-    HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-    PORT = 80  # Port to listen on (non-privileged ports are > 1023)
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # for debugging
         s.bind((HOST, PORT))
         s.listen()
         while True:
